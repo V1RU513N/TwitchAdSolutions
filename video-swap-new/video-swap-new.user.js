0399887333d446d389977d765cdb6d2a68b18ba6
Вирусиен <V12U513N@proton.me>
2025-06-21 14:40:03 +0200

    Update video-swap-new.user.js

--- video-swap-new/video-swap-new.user.js
+++ video-swap-new/video-swap-new.user.js
@@ -2,8 +2,8 @@
 // @name         TwitchAdSolutions (video-swap-new)
 // @namespace    https://github.com/pixeltris/TwitchAdSolutions
 // @version      1.35
-// @updateURL    https://github.com/pixeltris/TwitchAdSolutions/raw/master/video-swap-new/video-swap-new.user.js
-// @downloadURL  https://github.com/pixeltris/TwitchAdSolutions/raw/master/video-swap-new/video-swap-new.user.js
+// @updateURL    https://github.com/V1RU513N/TwitchAdSolutions/raw/master/video-swap-new/video-swap-new.user.js
+// @downloadURL  https://github.com/V1RU513N/TwitchAdSolutions/raw/master/video-swap-new/video-swap-new.user.js
 // @description  Multiple solutions for blocking Twitch ads (video-swap-new)
 // @author       pixeltris
 // @match        *://*.twitch.tv/*
